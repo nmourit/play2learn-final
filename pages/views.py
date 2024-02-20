074d132355c5797ac57d8d4b43dd5882d8928727
Nalee Mouritsen <nvang_1@hotmail.com>
2024-02-19 20:56:52 -0600

    added contact-us page and added project title to _base_vue.html

--- pages/views.py
+++ pages/views.py
@@ -2,3 +2,6 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'pages/home.html'
+
+class ContactUsView(TemplateView):
+    template_name = 'pages/contact_us.html'
