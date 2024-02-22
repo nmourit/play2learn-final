b042494dde43ec23b89a09fb7ecea4ff66fdcb08
Nalee Mouritsen <nvang_1@hotmail.com>
2024-02-22 17:33:34 -0600

    edited views.py

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic import DetailView
 
-# Create your views here.
+from .models import Review
+
+class ReviewDetailView(DetailView):
+    model = Review
+
