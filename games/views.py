317ac797fbc6d0827b3efb00f987cdbbb3390554
Nalee Mouritsen <nvang_1@hotmail.com>
2024-02-23 22:07:26 -0600

    moved games into a games folder

--- games/views.py
+++ games/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class MathFactsView(TemplateView):
-    template_name = "math-facts.html"
+    template_name = "games/math-facts.html"
 
 class AnagramHuntView(TemplateView):
-    template_name = "anagram-hunt.html"
+    template_name = "games/anagram-hunt.html"
--- reviews/models.py
+++ reviews/models.py
@@ -1,13 +1,25 @@
 from django.db import models
 from django.urls import reverse
 
+from common.utils.text import unique_slug
+
 class Review(models.Model):
     review = models.TextField(max_length=500)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(
+        max_length=50, unique=True, null=False, editable=False
+    )
 
     def get_absolute_url(self):
-        return reverse('review:detail', args=[str(self.pk)])
+        return reverse('review:detail', args=[str(self.slug)])
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            value = str(self)
+            self.slug = unique_slug(value, type(self))
+
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.review
