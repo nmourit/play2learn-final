dc7893b4baa10f495b6181952bb1134712f1f717
Nalee Mouritsen <nvang_1@hotmail.com>
2024-03-04 20:25:34 -0600

    added carousel to home page

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,15 @@
 from django.views.generic import TemplateView
 
+from reviews.models import Review
+
 class HomePageView(TemplateView):
     template_name = 'pages/home.html'
+    model = Review
+    print("class")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['reviews'] = Review.objects.all()
+        return context
+        
+
