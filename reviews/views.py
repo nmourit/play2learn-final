05957b37acb5e191796ebf3ca05dabdd950ab9f3
Nalee Mouritsen <nvang_1@hotmail.com>
2024-03-10 00:16:35 -0600

    changed reviews to be seen by user who created the review

--- reviews/views.py
+++ reviews/views.py
@@ -1,10 +1,9 @@
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.db.models import Q
 from django.urls import reverse_lazy
 from django.views.generic import (
-    CreateView, DeleteView, DetailView, ListView, UpdateView
+    CreateView, DeleteView, DetailView, TemplateView, UpdateView
 )
 
 from .models import Review
@@ -38,8 +37,14 @@ class ReviewDeleteView(UserPassesTestMixin, DeleteView):
 class ReviewDetailView(DetailView):
     model = Review
 
-class ReviewListView(ListView):
-    model = Review
+class ReviewListView(TemplateView):
+    template_name = "reviews/review_list.html"
+
+    def get_context_data(self, **kwargs):
+        username = self.request.user.username
+        context = super().get_context_data(**kwargs)
+        context['reviews'] = Review.objects.filter(user__username=username)
+        return context
 
 class ReviewUpdateView(SuccessMessageMixin, UserPassesTestMixin, UpdateView):
     model = Review
